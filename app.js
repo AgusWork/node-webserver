ca16f89bfcabe745ba549fbd49a849b84fae8e3a
Agustin Maria Cordoba <agustin.cordoba.work@gmail.com>
2024-08-14 11:39:17 -0300

    web server

--- app.js
+++ app.js
@@ -1,45 +1,43 @@
-require("dotenv").config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 3000;
-const hbs = require('hbs');
+const hbs = require("hbs");
 
-app.use(express.static("public") )
+app.use(express.static("public"));
 
-app.set('view engine', 'hbs');
-hbs.registerPartials(__dirname + '/views/partials', function (err) {
+app.set("view engine", "hbs");
+hbs.registerPartials(__dirname + "/views/partials", function (err) {
   if (err) {
-      console.error("Error loading partials:", err);
+    console.error("Error loading partials:", err);
   }
-})
-
-
+});
 
 app.get("/", function (req, res) {
-    res.render("home", {
-      nombre:"Agustin",
-      titulo:"PRUEBA HBS"
-    })
-  })
+  res.render("home", {
+    nombre: "Agustin",
+    titulo: "PRUEBA HBS",
+  });
+});
 app.get("/elements", function (req, res) {
-    res.render("elements", {
-      nombre:"Agustin",
-      titulo:"PRUEBA HBS"
-    })
-  })
+  res.render("elements", {
+    nombre: "Agustin",
+    titulo: "PRUEBA HBS",
+  });
+});
 app.get("/generic", function (req, res) {
-    res.render("generic", {
-      nombre:"Agustin",
-      titulo:"PRUEBA HBS"
-    })
-  })
+  res.render("generic", {
+    nombre: "Agustin",
+    titulo: "PRUEBA HBS",
+  });
+});
 
-  app.get('*', function (req, res) {
-    res.sendFile( __dirname + "/public/back/404.html")
-  })
-  
-app.listen(port,  '0.0.0.0', () => {
-    console.log(`Example app listenting at http://localhost:${port}/`)
-})
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/public/back/404.html");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Example app listenting at http://localhost:${port}/`);
+});
 
 module.exports = app;
