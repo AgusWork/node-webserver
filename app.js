1a0620b53702a2a52b098cea615c90f5cafde63d
Agustin Maria Cordoba <agustin.cordoba.work@gmail.com>
2024-08-14 02:10:35 -0300

    web server

--- app.js
+++ app.js
@@ -4,36 +4,41 @@ const app = express()
 const port = process.env.PORT || 3000;
 const hbs = require('hbs');
 
+app.use(express.static("public") )
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials', function (err) {
-    if (err) {
-        console.error("Error loading partials:", err);
-    }
-});
+  if (err) {
+      console.error("Error loading partials:", err);
+  }
+})
 
-app.use(express.static("public"));
 
-const renderPage = (res, view, nombre, titulo) => {
-    res.render(view, { nombre, titulo });
-};
 
 app.get("/", function (req, res) {
-    renderPage(res, "home", "Agustin", "PRUEBA HBS");
-});
-
+    res.render("home", {
+      nombre:"Agustin",
+      titulo:"PRUEBA HBS"
+    })
+  })
 app.get("/elements", function (req, res) {
-    renderPage(res, "elements", "Agustin", "PRUEBA HBS");
-});
-
+    res.render("elements", {
+      nombre:"Agustin",
+      titulo:"PRUEBA HBS"
+    })
+  })
 app.get("/generic", function (req, res) {
-    renderPage(res, "generic", "Agustin", "PRUEBA HBS");
-});
+    res.render("generic", {
+      nombre:"Agustin",
+      titulo:"PRUEBA HBS"
+    })
+  })
 
-app.get('*', function (req, res) {
-    res.sendFile(__dirname + "/public/404.html");
-});
+  app.get('*', function (req, res) {
+    res.sendFile( __dirname + "/public/404.html")
+  })
+  
+app.listen(port,  '0.0.0.0', () => {
+    console.log(`Example app listenting at http://localhost:${port}/`)
+})
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`App listening at http://localhost:${port}/`);
-});
